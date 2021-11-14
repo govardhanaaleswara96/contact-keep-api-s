cdb8b5e25187d3f64f0946db315754120c74e37a
govardhan <govardhanaaleswara88@gmail.com>
2021-11-14 10:53:37 +0530

    contacts route updated

--- routes/contacts.js
+++ routes/contacts.js
@@ -52,20 +52,60 @@ router.get('/', authMiddleware, async (req, res) => {
     }
 })
 
-// @route   PUT api/contacts
+// @route   PUT api/contacts/:id
 // @desc    update contacts
 // @access  private
 
-router.put('/', (req, res) => {
-    res.send("update Contact")
-})
+router.put('/:id', authMiddleware, async (req, res) => {
+    const userId = req.user.id;
+    const id = req.params.id;
+    const { name, email, phone, type } = req.body;
+    const contactFields = {};
+    if (name) contactFields.name = name;
+    if (email) contactFields.email = email;
+    if (phone) contactFields.phone = phone;
+    if (type) contactFields.type = type;
+    try {
+        let contact = await contactModel.findById(id);
+        if (!contact) {
+            return res.status(404).json({ msg: "Contact not found" });
+        }
+        let user = contact.user;
+        // make sure users owns contact
+        if (user.toString() !== userId) {
+            return res.status(401).json({ msg: "Not authorized" });
+        }
+        contact = await contactModel.findByIdAndUpdate(id, { $set: contactFields }, { new: true });
+        res.status(200).json(contact)
+    } catch (error) {
+        console.log(error.message);
+        return res.status(400).json({ msg: "Server Error" });
+    }
+});
 
-// @route   DELETE api/contacts
+
+// @route   DELETE api/contacts/:id
 // @desc    delete contacts
 // @access  private
 
-router.delete('/', (req, res) => {
-    res.send("delete Contact")
+router.delete('/:id', authMiddleware, async (req, res) => {
+    const userId = req.user.id;
+    const id = req.params.id;
+    try {
+        let contact = await contactModel.findById(id);
+        if (!contact) {
+            return res.status(404).json({ msg: "Contact not found" });
+        }
+        let user = contact.user;
+        // make sure users owns contact
+        if (user.toString() !== userId) {
+            return res.status(401).json({ msg: "Not authorized" });
+        }
+        await contactModel.findByIdAndRemove(id);
+        res.status(200).json({ msg: "contact removed" })
+    } catch (error) {
+        console.log(error.message);
+        return res.status(400).json({ msg: "Server Error" });
+    }
 })
-
 module.exports = router;
